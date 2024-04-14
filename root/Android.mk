b4df13b5cce6e71cb036b000be5d7b8f23618a66
Muhammad <muhammad23012009@hotmail.com>
2024-04-14 16:38:38 +0500

    root: add init script for ubuntu * needed to start minimediaservice on boot
    Signed-off-by: Muhammad <muhammad23012009@hotmail.com>

--- root/Android.mk
+++ root/Android.mk
@@ -75,3 +75,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.init_wlan_bt.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.ubuntu.rc
+LOCAL_MODULE_TAGS  := optional 
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.ubuntu.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
